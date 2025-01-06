d69f35adfdd4c265ac8db894638077db9ab6a65e
Dmitry-Ge <113944702+Dmitry-Ge@users.noreply.github.com>
2025-01-06 19:30:57 +0300

    Update processor.py

--- processor.py
+++ processor.py
@@ -1,11 +1,11 @@
 from opensearchpy import OpenSearch
 import os
+import sys
 import json
 import datetime
 
-
+filename = os.path.join(os.path.dirname(__file__), "pmacct.json")
 indexname = "pmacct-" + datetime.datetime.now().strftime("%Y-%m-%d")
-gmt = datetime.datetime.now() - datetime.timedelta(hours=3)
 
 es = OpenSearch(
     hosts=[{"host": "localhost", "port": 9200}],
@@ -14,18 +14,19 @@ es = OpenSearch(
     verify_certs=False,
 )
 
+# will be get lines of jsons from stdin
 data = []
+gmt = datetime.datetime.now() - datetime.timedelta(hours=3)
+
 for line in sys.stdin:
+    # print(line)
     data.append(line)
 
 
 for line in data:
-    res = json.loads(line)
-    res["datetime"] = gmt
-
     try:
-        es.index(index=indexname, body=res)
-    except Exception as e:
+        es.index(index=indexname, body={**json.loads(line), "datetime": gmt})
+    except (json.JSONDecodeError, Exception) as e:
         print(f"Error: {e} in line: {line}")
 
 es.close()
